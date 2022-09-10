8448136c47f4c5dc7e08d19e1662fea8851065d2
samirm00 <sammoham.be@gmail.com>
2022-09-10 21:02:50 +0200

    add dark theme

--- 2-core/1-think-functionally.js
+++ 2-core/1-think-functionally.js
@@ -1,1 +1,14 @@
-// page 
+// Think functionally 
+
+// Problem one : 
+/*
+    Let us see we have an e-commerce site, the user can fill his shopping card details 
+    and then click on Pay button. In this example the user shouldn't be able to click twice 
+    otherwise, he will pay billed several times.
+
+    How can we solve this problem? 
+
+*/
+
+// DOM ELEMENT
+var payBtn = document.getElementById("payBtn");
